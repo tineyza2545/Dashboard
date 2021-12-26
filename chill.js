265575346433ec04de5eeb85309d37fab09317ab
tineyza2545 <tineyza2545@gmail.com>
2021-12-26 21:39:46 +0700

    first commit

---
+++ chill.js
@@ -0,0 +1,64 @@
+var app = new Vue({
+    el: '#new1',
+    data: {
+      name: 'Suppakorn Sungpom',
+      name1:'interesentiem'
+    }
+  })
+  var uup = new Vue({
+    el: '#new2',
+    data: {
+      name1:'interesentiem',
+      name3:'not interested',
+      name4:'visitors',
+      name5:'recommend',
+      name6:'60%',
+      name7:'30%',
+      name8:'10%',
+      name9:'0%'
+  
+    }
+  })
+  var oro = new Vue({
+    el: '#neww1',
+    data: {
+      name10:'percent',
+  
+    }
+  })
+  var mmr = new Vue({
+    el: '#neww2',
+    data: {
+      name11:'interesentiem',
+      name12:'not interested',
+      name13:'visitors'
+      
+    }
+  })
+  var app = new Vue({
+    el: '#neww3',
+    data: {
+     name14:'Percentage Rate',
+     name15:'Instagram',
+     name16:'Facebook',
+     name17:'Twitter',
+     name18:'Line',
+     name19:'Google'
+    }
+  })
+  var app = new Vue({
+    el: '#neww4',
+    data: {
+      name20:'Google',
+      name21:'Line',
+      name22:'Twitter',
+      name23:'Facebook',
+      name24:'Instagram'
+    }
+  })
+  var app = new Vue({
+    el: '#neww5',
+    data: {
+     name25:'My profile'
+    }
+  })
